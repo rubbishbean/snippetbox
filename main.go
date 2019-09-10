ae8a3943aa59b8c2d201c69697abf7562f2d8ef0
Yan, Jiaxing (CORP) <Jiaxing.Yan@adp.com>
2019-09-09 23:29:06 -0400

    Add 2 more request routings

--- main.go
+++ main.go
@@ -8,7 +8,19 @@ import (
 // Define a home handler function which writes a byte slice containing
 // "Hello from Snippetbox" as the response body.
 func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
 	w.Write([]byte("Hello from Snippetbox"))
+}
+
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet..."))
+}
+
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create a new snippet..."))
 }
 
 func main() {
@@ -16,7 +28,9 @@ func main() {
 	// register the home function as the handler for the "/" URL pattern.
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
-	
+	mux.HandleFunc("/snippet", showSnippet)	
+	mux.HandleFunc("/snippet/create", createSnippet)
+
 	// http.ListenAndServe() -> start a new web server
 	// pass in 2 params: TCP address to listen on and servemux.
 	// error logged by log.Fatal()
